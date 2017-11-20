c5ca620d209dcdf766cf0c8bc7dded95d20163c1
Jordach <jordach.snelling@gmail.com>
2017-11-20 16:53:41 +0000

    up the gain of sounds by 0.05

--- mods/core/sounds.lua
+++ mods/core/sounds.lua
@@ -4,7 +4,7 @@ mcore.sound_glass = {
 
 	footstep = {name = "", gain = 0},
 	
-	dug = {name = "core_glass_dug", gain = 0.15},
+	dug = {name = "core_glass_dug", gain = 0.25},
 	
 	place = {name = "", gain = 0},
 
@@ -12,9 +12,9 @@ mcore.sound_glass = {
 
 mcore.sound_grass = {
 
-	footstep = {name = "core_plants_dug", gain = 0.03},
+	footstep = {name = "core_plants_dug", gain = 0.08},
 	
-	dug = {name = "core_grass_dug", gain = 0.25},
+	dug = {name = "core_grass_dug", gain = 0.25+0.05},
 	
 	place = {name = "core_grass_dug", gain = 0.25},
 
@@ -22,80 +22,80 @@ mcore.sound_grass = {
 
 mcore.sound_dirt = {
 
-	footstep = {name = "core_grass_dug", gain = 0.12},
+	footstep = {name = "core_grass_dug", gain = 0.12+0.05},
 	
-	dug = {name = "core_grass_dug", gain = 0.25},
+	dug = {name = "core_grass_dug", gain = 0.25+0.05},
 	
-	place = {name = "core_grass_dug", gain = 0.25},
+	place = {name = "core_grass_dug", gain = 0.25+0.05},
 	
 }
 
 mcore.sound_gravel = {
 
-	footstep = {name = "core_gravel_footstep", gain = 0.1},
+	footstep = {name = "core_gravel_footstep", gain = 0.1+0.05},
 	
-	dug = {name = "core_gravel_footstep", gain = 0.15},
+	dug = {name = "core_gravel_footstep", gain = 0.15+0.05},
 	
-	place = {name = "core_gravel_footstep", gain = 0.15},
+	place = {name = "core_gravel_footstep", gain = 0.15+0.05},
 
 }
 
 mcore.sound_metallic = {
 
-	footstep = {name = "core_metallic_footstep", gain = 0.12},
+	footstep = {name = "core_metallic_footstep", gain = 0.12+0.05},
 	
-	dug = {name = "core_metallic_dug", gain = 0.15},
+	dug = {name = "core_metallic_dug", gain = 0.15+0.05},
 	
-	place = {name = "core_metallic_dug", gain = 0.15},
+	place = {name = "core_metallic_dug", gain = 0.15+0.05},
 
 }
 
 mcore.sound_plants = {
 
-	footstep = {name = "core_plants_dug", gain = 0.03},
+	footstep = {name = "core_plants_dug", gain = 0.03+0.05},
 	
-	dug = {name = "core_plants_dug", gain = 0.15},
+	dug = {name = "core_plants_dug", gain = 0.15+0.05},
 	
-	place = {name = "core_plants_dug", gain = 0.15},
+	place = {name = "core_plants_dug", gain = 0.15+0.05},
 
 }
 
 mcore.sound_sand = {
 
-	footstep = {name = "core_sand_footstep", gain = 0.1},
+	footstep = {name = "core_sand_footstep", gain = 0.1+0.05},
 	
-	dug = {name = "core_sand_dug", gain = 0.15},
+	dug = {name = "core_sand_dug", gain = 0.15+0.05},
 	
-	place = {name = "core_sand_dug", gain = 0.15},
+	place = {name = "core_sand_dug", gain = 0.15+0.05},
 
 }
 
 mcore.sound_snow = {
 
-	footstep = {name = "core_snow_footstep", gain = 0.1},
+	footstep = {name = "core_snow_footstep", gain = 0.1+0.05},
 	
-	dug = {name = "core_snow_dug", gain = 0.15},
+	dug = {name = "core_snow_dug", gain = 0.15+0.05},
 	
-	place = {name = "core_snow_footstep", gain = 0.15},
+	place = {name = "core_snow_footstep", gain = 0.15+0.05},
 
 }
 
 mcore.sound_stone = {
 
-	footstep = {name = "core_stone_footstep", gain = 0.12},
+	footstep = {name = "core_stone_footstep", gain = 0.12+0.05},
 	
-	dug = {name = "core_stone_dug", gain = 0.15},
+	dug = {name = "core_stone_dug", gain = 0.15+0.05},
 	
-	place = {name = "core_stone_dug", gain = 0.15},
+	place = {name = "core_stone_dug", gain = 0.15+0.05},
 
 }
 
 mcore.sound_wood = {
 
-	footstep = {name = "core_wood_footstep", gain = 0.12},
+	footstep = {name = "core_wood_footstep", gain = 0.12+0.05},
 
-	dug = {name = "core_wood_dug", gain = 0.3},
+	dug = {name = "core_wood_dug", gain = 0.3+0.05},
 	
-	place = {name = "core_wood_dug", gain = 0.3},
+	place = {name = "core_wood_dug", gain = 0.3+0.05},
 
 }
