795b122b5f6a1de33a928c24dda223751eebe529
Jordach <jordach.snelling@gmail.com>
2017-12-11 17:38:29 +0000

    remove debug statements

--- mods/core/init.lua
+++ mods/core/init.lua
@@ -175,17 +175,11 @@ function mcore.sensible_facedir(itemstack, placer, pointed_thing)
 
 	local pz = math.abs(placer:get_pos().z - rpos.z)
 	
-	print(px)
-	print(pz)
-	
-	
 	if px < 2 and pz < 2 then -- if the node is being placed 1 block away from us, then lets place it either upright or upside down
 		
 		local pY = math.abs(placer:get_pos().y + 1.14) -- we measure the y distance by itself as it may not be needed for wall placed blocks.
 		
-		print(pY)
-		
-		if pY - math.abs(rpos.y) > 1 then -- are we being placed on the floor? let's be upright then.
+		if pY - math.abs(rpos.y) > 2 then -- are we being placed on the floor? let's be upright then.
 				
 			if deg_to_fdir == 0 then fdir = 0 -- north
 			elseif deg_to_fdir == 1 then fdir = 3 --east
